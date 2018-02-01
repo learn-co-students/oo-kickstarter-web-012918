a7524e1fc0d499c76b1f17ac0a4ad9a652169297
jaf7 <janthonyfields@gmail.com>
2018-02-01 16:49:33 -0500

    built out co-related classes that interact

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class Project
+  attr_reader :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    # backer.back_project(self)
+    backer.backed_projects << self
+    # binding.pry
+  end
+
+end
